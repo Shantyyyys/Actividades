ba74c93d30ce9d0c278febaa4502acb0c6dc2bde
Shantyyyys <santymv54segunda@gmail.com>
2026-04-27 21:30:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/actividades/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.actividades
+
+import android.content.Intent
+import com.example.actividades.databinding.ActivityMainBinding
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnEnviar.setOnClickListener {
+            val intent = Intent(this, secundaryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
